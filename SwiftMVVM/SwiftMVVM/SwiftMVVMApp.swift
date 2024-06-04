a2f60d83f85ce0546821c5a23afa0bf03cf49082
MaverickHitesh <hitesh.135288@gmail.com>
2024-06-04 14:25:02 +0530

    swift xcode project commit

---
+++ SwiftMVVM/SwiftMVVM/SwiftMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMVVMApp.swift
+//  SwiftMVVM
+//
+//  Created by Hitesh Singh on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
